5fe7e1a62e5ed70f93996be95a086dcc579c2d70
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-30 03:58:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ class CashRegister
     @items << item
     # quantity.times do 
     #   @items << item
-    end
+    # end
     @price = price
     @quantity = quantity 
     @total += (price * quantity)
